1301174bc06e30bd984908cabf7c9f973b1f55ca
Walid10010 <107435785+Walid10010@users.noreply.github.com>
2024-04-11 23:31:52 +0200

    update

--- experiment.py
+++ experiment.py
@@ -14,7 +14,7 @@ from sklearn.metrics import roc_auc_score
 import glob
 
 
-algo_dic ={'ADERH':ADERH, 'INNE':INNE, 'IForest':IForest, 'DIF':DIF, 'LOF':LOF, 'LODA': LODA, 'SO_GAAL':SO_GAAL, 'RCA':RCA, 'DBSCAN':DBSCAN, 'OCSVM':OCSVM }
+algo_dic ={'ADERH':ADERH, 'INNE':INNE, 'IForest':IForest, 'DIF':DIF, 'LOF':LOF, 'LODA': LODA, 'SO_GAAL':SO_GAAL, 'RCA':RCA, 'RDP':RDP, 'DBSCAN':DBSCAN, 'OCSVM':OCSVM }
 
 import glob
 import numpy as np
@@ -23,6 +23,7 @@ import numpy as np
 random_seeds = [0, 1, 2, 3, 4, 5, 10, 100, 1000, 10000]
 
 for data_name in glob.glob('Dataset/*'):
+    if 'reast'  not in data_name: continue
     for algo_name in algo_dic:
 
         data = np.load('{}'.format(data_name),
@@ -35,11 +36,25 @@ for data_name in glob.glob('Dataset/*'):
              algo.fit(X)
              outlier_score, label = algo.decision_function(X), algo.labels_
              roc_score_value += roc_auc_score(Y, outlier_score) /len(random_seeds)
+        elif  algo_name in  ['RCA', 'RDP'] :
+            algo = algo_dic[algo_name](device='cpu')
+            algo.fit(X)
+            outlier_score, label = algo.decision_function(X), algo.predict(X)
+            roc_score_value = roc_auc_score(Y, outlier_score)
+
+        elif  algo_name in ['DBSCAN']:
+            algo = DBSCAN()
+            y_pred = y_prob = algo.fit_predict(X)
+            y_pred[y_pred >= 0] = -2
+            y_pred[y_pred == -1] = 1
+            y_pred[y_pred == -2] = 0
+            roc_score_value = roc_auc_score(Y, y_pred)
+
         else:
              algo = algo_dic[algo_name]()
              algo.fit(X)
              outlier_score, label = algo.decision_function(X), algo.labels_
-             roc_score_value = roc_auc_score(Y, outlier_score) / len(random_seeds)
+             roc_score_value = roc_auc_score(Y, outlier_score)
 
         print('dataset:{}, algo: {}, roc-score: {} '.format(data_name.split('/')[-1].split('.')[0], algo_name,   roc_score_value))
 
